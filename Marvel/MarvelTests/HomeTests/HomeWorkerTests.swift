d33b12a970d471ba8390479424103d05eaa58f42
Vitor Costa <deividevitor565@gmail.com>
2020-07-27 16:56:56 -0300

    fixed tests errors

--- Marvel/MarvelTests/HomeTests/HomeWorkerTests.swift
+++ Marvel/MarvelTests/HomeTests/HomeWorkerTests.swift
@@ -24,40 +24,55 @@ class HomeWorkerTests: XCTestCase {
 
     func testIfTheFuncDecodeItsWorks() {
         guard let data = """
-            [
             {
-              "thumbnail" : {
-                "path" : "http:",
-                "extension" : "jpg"
-              },
-              "comics" : {
-                "returned" : 12,
-                "collectionURI" : "http:",
-                "items" : [
-                  {
-                    "name" : "Avengers: The Initiative (2007) #14",
-                    "resourceURI" : "http:"
-                  }],
-                "available" : 12
-              },
-              "series" : {
-                "returned" : 3,
-                "collectionURI" : "http:",
-                "items" : [
-                  {
-                    "name" : "Avengers: The Initiative (2007 - 2010)",
-                    "resourceURI" : "http:"
-                  }],
-                "available" : 3
-              },
-              "id" : 1011334,
-              "resourceURI" : "http:",
-              "description" : "",
-              "modified" : "2014-04-29T14:18:17-0400",
-              "name" : "3-D Man"
-              }
+            "status" : "Ok",
+            "data" : {
+              "results":
+                [
+                {
+                  "thumbnail" : {
+                    "path" : "http:",
+                    "extension" : "jpg"
+                  },
+                  "comics" : {
+                    "returned" : 12,
+                    "collectionURI" : "http:",
+                    "items" : [
+                      {
+                        "name" : "Avengers: The Initiative (2007) #14",
+                        "resourceURI" : "http:"
+                      }],
+                    "available" : 12
+                  },
+                  "series" : {
+                    "returned" : 3,
+                    "collectionURI" : "http:",
+                    "items" : [
+                      {
+                        "name" : "Avengers: The Initiative (2007 - 2010)",
+                        "resourceURI" : "http:"
+                      }],
+                    "available" : 3
+                  },
+                  "id" : 1011334,
+                  "resourceURI" : "http:",
+                  "description" : "",
+                  "modified" : "2014-04-29T14:18:17-0400",
+                  "name" : "3-D Man"
+                  }
+                }
+                ],
+              "offset" : 0,
+              "count" : 20,
+              "total" : 1493,
+              "limit" : 20
+            },
+            "code" : 200,
+            "copyright" : "© 2020 MARVEL",
+            "attributionText" : "Data provided by Marvel. © 2020 MARVEL",
+            "attributionHTML" : "<a href=\"http:",
+            "etag" : "5943fb7b0894f1123002ad16593b0da8cd1fd1ac"
             }
-            ]
         """.data(using: .utf8) else { return }
         response = worker?.decode(data: data)
         XCTAssertNotNil(response)
