c6fd04138b9c3581bb22fed15d7ddd5c5ae3502e
Vitor Costa <deividevitor565@gmail.com>
2020-07-27 16:50:21 -0300

    created comics type

--- Marvel/Marvel/Models/Comics.swift
+++ Marvel/Marvel/Models/Comics.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+
+struct Comics: Codable{
+    var id: Int
+    var title: String
+    var description: String
+    var modified: String
+    var thumbnail: Thumbnail
+    var creators: Creators
+    var prices: [Prices]
+}
+
+struct Creators: Codable {
+    var available: Int
+    var items: [Creator]
+}
+
+struct Creator: Codable {
+    var name: String
+    var role: String
+}
+
+struct Prices: Codable {
+    var type: String
+    var price: Double
+}
