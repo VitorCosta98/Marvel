f8f1e1da7a7657e90da727519a5d3b6661899ff9
Vitor Costa <deividevitor565@gmail.com>
2020-07-27 16:51:01 -0300

    created worker to deailsView

--- Marvel/Marvel/Details/DetailsWorker.swift
+++ Marvel/Marvel/Details/DetailsWorker.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+protocol DetailsWorkerProtocol {
+    func makeGETRequest(urlString: String, completion: @escaping(APIResponseComics) -> Void)
+    func decode(data: Data) -> APIResponseComics?
+}
+
+class DetailsWorker: DetailsWorkerProtocol {
+    
+    func makeGETRequest(urlString: String, completion: @escaping(APIResponseComics) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: nil)
+        session.dataTask(with: url) { data, response, error in
+            guard let data = data else { return }
+            
+            do{
+                if let jsonObject = try JSONSerialization.jsonObject(with: data, options: []) as? [String: AnyObject] {
+                    let jsonData = try JSONSerialization.data(withJSONObject: jsonObject as Any, options: .prettyPrinted)
+                    guard let apiResponse = self.decode(data: jsonData) else { return }
+                    print(apiResponse)
+                    //to call completion
+                }
+            } catch {
+                error.localizedDescription
+            }
+            
+        }.resume()
+    }
+    
+    func decode(data: Data) -> APIResponseComics? {
+        return try? JSONDecoder().decode(APIResponseComics.self, from: data)
+    }
+}
