2f379d2ca9159ac511e29e4fdd82f8d1a179fea0
Vitor Costa <deividevitor565@gmail.com>
2020-07-27 13:45:05 -0300

    added a test related to the presenter

--- Marvel/MarvelTests/HomeTests/HomeInteractorTests.swift
+++ Marvel/MarvelTests/HomeTests/HomeInteractorTests.swift
@@ -12,15 +12,18 @@ import XCTest
 class HomeInteractorTests: XCTestCase {
     var worker: MockHomeWorker?
     var interactor: HomeInteractor?
+    var presenter: MockPresenter?
     
     override func setUp() {
         worker = MockHomeWorker()
-        interactor = HomeInteractor(worker: worker!)
+        presenter = MockPresenter()
+        interactor = HomeInteractor(worker: worker!, presenter: presenter!)
     }
 
     override func tearDown() {
         worker = nil
         interactor = nil
+        presenter = nil
     }
 
     func testIfTheInteractorIsCallingTheWorker() {
@@ -28,18 +31,48 @@ class HomeInteractorTests: XCTestCase {
         guard let worker = worker else { return }
         XCTAssertTrue(worker.makeGETRequestWasCalled)
     }
+    
+    func testIfTheInteractorIsCallingThePresenter() {
+        interactor?.onViewLoad()
+        guard let presenter = presenter else { return }
+        XCTAssertTrue(presenter.showWasCalled)
+    }
 }
 
 class MockHomeWorker: HomeWorkerProtocol {
     var decodeWasCalled = false
     var makeGETRequestWasCalled = false
+    var response: APIResponse?
     
     func makeGETRequest(urlString: String, completion: @escaping (APIResponse) -> Void) {
         makeGETRequestWasCalled = true
+        
+        var characters: [Character] = []
+        let thumb = Thumbnail(path: "", ext: "")
+        let items = [Items(name: "", resourceURI: "")]
+        let relatedWorks = RelatedWorks(returned: 0, items: items, available: 0)
+        let character1 = Character(id: 0, name: "", thumbnail: thumb, comics: relatedWorks, series: relatedWorks)
+        let character2 = Character(id: 0, name: "", thumbnail: thumb, comics: relatedWorks, series: relatedWorks)
+        characters = [character1, character2]
+        let apiData = APIData(offset: 0, limit: 0, total: 0, count: 0, results: characters)
+        response = APIResponse(code: 0, status: "", data: apiData)
+        characters = [character1, character2]
+        guard let response = response else { return }
+        
+        completion(response)
     }
 
     func decode(data: Data) -> APIResponse? {
         decodeWasCalled = true
         return nil
+    }
+}
+
+class MockPresenter: HomePresenterProtocol {
+    var view: HomeViewControlerProtocol?
+    var showWasCalled = false
+    
+    func show(characters: [Character]) {
+        showWasCalled = true
     }
 }
