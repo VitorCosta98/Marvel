58f000b42e59466e4d4f188817a427f516d57aaf
Vitor Costa <deividevitor565@gmail.com>
2020-07-28 17:48:08 -0300

    added title to navBar

--- Marvel/Marvel/AppDelegate.swift
+++ Marvel/Marvel/AppDelegate.swift
@@ -24,6 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         controller.interactor = interactor
         
         let nav = UINavigationController(rootViewController: controller)
+        nav.navigationBar.topItem?.title = "Marvel"
         window?.rootViewController = nav
         window?.makeKeyAndVisible()
         
