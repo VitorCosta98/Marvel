0ba9bf69296e558618cdacb8d286d8f35b52f40e
Vitor Costa <deividevitor565@gmail.com>
2020-07-28 03:53:03 -0300

    added extensions of collectionView, registers for it, protocol and show function

--- Marvel/Marvel/Details/DetailsViewController.swift
+++ Marvel/Marvel/Details/DetailsViewController.swift
@@ -8,23 +8,67 @@
 
 import UIKit
 
-class DetailsViewController: UIViewController {
+protocol DetailsViewControlerProtocol {
+    var interactor: DetailsInteractorProtocol? { get set }
+    func show(comics: [Comics])
+}
 
+class DetailsViewController: UIViewController, DetailsViewControlerProtocol {
+    @IBOutlet weak var imageCharacter: UIImageView!
+    @IBOutlet weak var nameCharacter: UILabel!
+    @IBOutlet weak var collectionComics: UICollectionView!
+    @IBOutlet weak var collectionSeries: UICollectionView!
+    var interactor: DetailsInteractorProtocol?
+    var comics: [Comics] = []
+    
+    //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let nibComicsCell = UINib(nibName: "ComicsViewCell", bundle: nil)
+        collectionComics.register(nibComicsCell, forCellWithReuseIdentifier: "ComicsViewCell")
+        let nibSeriesCell = UINib(nibName: "SeriesViewCell", bundle: nil)
+        collectionSeries.register(nibSeriesCell, forCellWithReuseIdentifier: "SeriesViewCell")
+        
+        collectionComics.delegate = self
+        collectionComics.dataSource = self
+        collectionSeries.delegate = self
+        collectionSeries.dataSource = self
+        interactor?.onViewLoad()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: Functions
+    func show(comics: [Comics]) {
+        self.comics = comics
+        DispatchQueue.main.async {
+            self.collectionComics.reloadData()
+        }
     }
-    */
+}
 
+//MARK: Extensions
+extension DetailsViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == collectionComics {
+            return comics.count
+        }
+        return comics.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == collectionComics {
+            if let comicCell = collectionView.dequeueReusableCell(withReuseIdentifier: "ComicsViewCell", for: indexPath) as? ComicsViewCell {
+                guard let urlImage = URL(string: "\(comics[indexPath.row].thumbnail.path).\(comics[indexPath.row].thumbnail.ext)") else { return UICollectionViewCell() }
+                comicCell.setup(imageURL: urlImage, title: comics[indexPath.row].title)
+                return comicCell
+            }
+        } else if collectionView == collectionSeries {
+            if let seriesCell = collectionView.dequeueReusableCell(withReuseIdentifier: "SeriesViewCell", for: indexPath) as? SeriesViewCell {
+                guard let urlImage = URL(string: "https://images-na.ssl-images-amazon.com/images/I/817R7vTuFwL.jpg") else { return UICollectionViewCell() }
+                seriesCell.setup(imageURL: urlImage, name: "some")
+                return seriesCell
+            }
+        }
+        return UICollectionViewCell()
+    }
 }
