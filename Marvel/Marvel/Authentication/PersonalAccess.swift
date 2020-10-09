69aca6d3c967a553874be59b2a970dfd64afb66e
Vitor Costa <deividevitor565@gmail.com>
2020-10-09 13:24:22 -0300

    created this enum to centralize the apiKey and privateKey

---
+++ Marvel/Marvel/Authentication/PersonalAccess.swift
@@ -0,0 +1,14 @@
+//
+//  PersonalAccess.swift
+//  Marvel
+//
+//  Created by Vitor Costa on 09/10/20.
+//  Copyright © 2020 Vitor Costa. All rights reserved.
+//
+
+import Foundation
+
+enum PersonalAccess: String {
+    case apiKey = "b64080ac39198f95abcdb20fc185b688"
+    case privateKey = "d4cce909b9e33cd8cb10d483c082a8fe2fca0322"
+}
